ec6947947d23a145c40653f04f60a35c5e8829a1
dungplt <pltdunggg@gmail.com>
2020-09-26 11:19:17 +0700

    Added button responses

--- app/src/main/java/com/hfad/locationdetector/MainActivity.java
+++ app/src/main/java/com/hfad/locationdetector/MainActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -15,6 +17,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.toolbox.Volley;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity implements RecyclerMainAdapter.ItemClickListener {
@@ -59,6 +63,28 @@ public class MainActivity extends Activity implements RecyclerMainAdapter.ItemCl
         super.onActivityResult(requestCode, resultCode, data);
         // TODO: Implement this method
         // Check if resultCode == RESULT_OK, then call the corresponding methods
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CAMERA_REQUEST:
+                    handleImageResponse(data);
+                    break;
+                case GALLERY_REQUEST:
+                    handleGalleryResponse(data);
+                    break;
+            }
+        }
+    }
+
+    private void handleGalleryResponse(Intent data) {
+        try {
+            final Uri imageUri = data.getData();
+            final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+            imagePath = imageUri.getPath();
+            currentImage = BitmapFactory.decodeStream(imageStream);
+            imageView.setImageBitmap(currentImage);
+        } catch (FileNotFoundException e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void sendImageToServer() {
@@ -115,5 +141,27 @@ public class MainActivity extends Activity implements RecyclerMainAdapter.ItemCl
     @Override
     public void onClick(int position) {
         // TODO: Implement this method
+        switch (position) {
+            case 0:
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+                break;
+            case 1:
+                Intent imageCaptureIntent = new Intent(this, ImageCaptureActivity.class);
+                startActivityForResult(imageCaptureIntent, CAMERA_REQUEST);
+                break;
+            case 2:
+                showSendButtonMessage();
+                sendImageToServer();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showSendButtonMessage() {
+        String msg = "Let us guess where it is...";
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 }
